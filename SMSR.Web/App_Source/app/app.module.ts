ae4a37c68165a5b6fd547bd6bb4fcf92a2311c92
Scott Palmateer <scott@universalrogue.com>
2018-06-10 15:58:52 -0400

    basic angular pieces are in place

---
+++ SMSR.Web/App_Source/app/app.module.ts
@@ -0,0 +1,16 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+    declarations: [
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [HomeComponent]
+})
+export class AppModule { }
---
+++ SMSR.Web/App_Source/app/home/home.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: 'App_Source/app/home/home.component.html'
+})
+export class HomeComponent {
+    name: string = "Scott";
+}
