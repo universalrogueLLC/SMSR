bb0bf67289ca8fa8f3be0c785e36721ea89ec087
Scott Palmateer <scott@universalrogue.com>
2018-06-16 00:11:16 -0400

    finished up navbar

--- SMSR.Web/App_Source/app/shell/shell.component.js
+++ SMSR.Web/App_Source/app/shell/shell.component.js
@@ -13,11 +13,14 @@ var __param = (this && this.__param) || function (paramIndex, decorator) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var ng_bootstrap_1 = require("@ng-bootstrap/ng-bootstrap");
 var appService_service_1 = require("../appService.service");
 var ShellComponent = (function () {
-    function ShellComponent(appService) {
+    function ShellComponent(appService, modalService) {
         this.appService = appService;
+        this.modalService = modalService;
         this.actions = new Array();
+        this.navDialogRef = null;
     }
     ShellComponent.prototype.ngOnInit = function () {
         var _this = this;
@@ -26,13 +29,38 @@ var ShellComponent = (function () {
             _this.actions = _this.appService.getAppActions(_this.me);
         });
     };
+    ShellComponent.prototype.openModal = function (dialog) {
+        this.navDialogRef = this.modalService.open(dialog, { size: "sm" });
+        this.navDialogRef.result.then(function (result) {
+        }, function (reason) {
+        });
+    };
+    ShellComponent.prototype.getDismissReason = function (reason) {
+        if (reason === ng_bootstrap_1.ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        }
+        else if (reason === ng_bootstrap_1.ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        }
+        else {
+            return "with: " + reason;
+        }
+    };
+    ShellComponent.prototype.navDialogNavigate = function (action) {
+        if (this.navDialogRef != null) {
+            this.navDialogRef.dismiss("Navigating: " + action.route);
+            document.location.href = "/#" + action.route;
+        }
+    };
     ShellComponent = __decorate([
         core_1.Component({
             selector: 'app-shell',
             templateUrl: 'App_Source/app/shell/shell.component.html'
         }),
         __param(0, core_1.Inject(core_1.forwardRef(function () { return appService_service_1.AppService; }))),
-        __metadata("design:paramtypes", [appService_service_1.AppService])
+        __param(1, core_1.Inject(core_1.forwardRef(function () { return ng_bootstrap_1.NgbModal; }))),
+        __metadata("design:paramtypes", [appService_service_1.AppService,
+            ng_bootstrap_1.NgbModal])
     ], ShellComponent);
     return ShellComponent;
 }());
--- SMSR.Web/App_Source/app/shell/shell.component.ts
+++ SMSR.Web/App_Source/app/shell/shell.component.ts
@@ -1,5 +1,7 @@
 ﻿import { Component, Inject, forwardRef, OnInit } from '@angular/core';
 
+import { NgbModal, NgbModalRef, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppService } from "../appService.service";
 import { User } from "../entities";
 import { AppAction } from "../types";
@@ -14,12 +16,42 @@ export class ShellComponent implements OnInit {
 
     actions: AppAction[] = new Array<AppAction>();
 
-    constructor( @Inject(forwardRef(() => AppService)) private appService: AppService) { }
+    navDialogRef: NgbModalRef = null;
+
+    constructor( @Inject(forwardRef(() => AppService)) private appService: AppService,
+        @Inject(forwardRef(() => NgbModal)) private modalService: NgbModal) { }
 
     ngOnInit(): void {
         this.appService.getMe().subscribe((user) => {
             this.me = user;
             this.actions = this.appService.getAppActions(this.me);
         });
+    }
+
+    openModal(dialog) {
+        this.navDialogRef = this.modalService.open(dialog, {size:"sm"});
+
+        this.navDialogRef.result.then((result) => {
+            //console.log( `Closed with: ${result}` );
+        }, (reason) => {
+            //console.log( `Dismissed ${this.getDismissReason(reason)}` );
+        });
+    }
+
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return `with: ${reason}`;
+        }
+    }
+
+    navDialogNavigate(action: AppAction): void {
+        if (this.navDialogRef != null) {
+            this.navDialogRef.dismiss("Navigating: " + action.route);
+            document.location.href = `/#${action.route}`;
+        }
     }
 }
